bbee086b4eebd4977542e8163afc274c91cff2e5
Raphael Berger <raphaelmb@gmail.com>
2022-04-21 13:10:25 -0300

    Promises

--- main.mjs
+++ main.mjs
@@ -1,24 +1,24 @@
 import Database from "./Database.mjs";
 
-try {
-  const database = new Database();
-  database.execute(
+const database = new Database();
+database
+  .execute(
     "create table author (id number, name string, age number, city string, state string, country string)"
-  );
-  database.execute(
-    "insert into author (id, name, age) values (1, Douglas Crockford, 62)"
-  );
-  database.execute(
-    "insert into author (id, name, age) values (2, Linus Torvalds, 47)"
-  );
-  database.execute(
-    "insert into author (id, name, age) values (3, Martin Fowler, 54)"
-  );
-  database.execute("delete from author where id = 2");
-  database.execute("select name, age from author");
-
-  const result = database.execute("select name, age from author");
-  console.log(JSON.stringify(result, undefined, " "));
-} catch (e) {
-  console.log(e.message);
-}
+  )
+  .then(() =>
+    Promise.all([
+      database.execute(
+        "insert into author (id, name, age) values (1, Douglas Crockford, 62)"
+      ),
+      database.execute(
+        "insert into author (id, name, age) values (2, Linus Torvalds, 47)"
+      ),
+      database.execute(
+        "insert into author (id, name, age) values (3, Martin Fowler, 54)"
+      ),
+    ])
+  )
+  // .then(() => database.execute("delete from author where id = 2"))
+  .then(() => database.execute("select name, age from author"))
+  .then((result) => console.log(JSON.stringify(result, undefined, " ")))
+  .catch((error) => console.log(error.message));
